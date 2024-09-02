6f86cfc44e2865d8ee68e279ac7b8171252f5b10
Simon Moynihan <simon.moynihan33@gmail.com>
2024-09-02 14:02:16 +0000

    Register comment model in admin.py

--- level_lounge/admin.py
+++ level_lounge/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import UserProfile, Post
+from .models import UserProfile, Post, comment
 
 # Register your models here.
 admin.site.register(UserProfile)
 admin.site.register(Post)
+admin.site.register(comment)
