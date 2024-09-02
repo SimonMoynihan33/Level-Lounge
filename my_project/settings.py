c1a4e75227f9f9bf54af7422fba96ad69d0f5eea
Simon Moynihan <simon.moynihan33@gmail.com>
2024-09-02 11:02:10 +0000

    Migrate UserProfile model and register in admin.py

--- my_project/settings.py
+++ my_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-simonmoynih-levellounge-hzs6s09zeua.ws.codeinstitute-ide.net',
                 '.herokuapp.com']
@@ -97,6 +97,12 @@ WSGI_APPLICATION = 'my_project.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com"
+]
 
 
 # Password validation
