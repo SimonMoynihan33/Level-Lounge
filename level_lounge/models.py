ad97677c7e97e5db5ec7030a37f258a25d8bae2d
Simon Moynihan <simon.moynihan33@gmail.com>
2024-09-02 11:26:36 +0000

    Migrate Post model and register

--- level_lounge/models.py
+++ level_lounge/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
 # Create your models here.
 class UserProfile(models.Model):
     """
@@ -46,4 +50,7 @@ class Post(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
+    status = models.IntegerField(choices=STATUS, default=0)
