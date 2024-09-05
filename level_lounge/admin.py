f6fdbc0b2702f91760906747024f24edf56ac2ff
Simon Moynihan <simon.moynihan33@gmail.com>
2024-09-05 09:45:25 +0000

    Fix spelling error on post model. exercpt to excerpt

--- level_lounge/admin.py
+++ level_lounge/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 from .models import UserProfile, Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    list_display = ('title', 'slug', 'status', 'created_at')
+    search_fields = ['title', 'content']
+    list_filter = ('status', 'created_at')
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
+
 
 # Register your models here.
 admin.site.register(UserProfile)
-admin.site.register(Post)
 admin.site.register(Comment)
