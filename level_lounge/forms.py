eba90d9c26aae9453e7c17232d4d824cdf606868
Simon Moynihan <simon.moynihan33@gmail.com>
2024-09-19 12:40:45 +0000

    Replies and comments styling. Style forms for post creation and login/out

--- level_lounge/forms.py
+++ level_lounge/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from .models import Comment, Post
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class CommentForm(forms.ModelForm):
     class Meta:
