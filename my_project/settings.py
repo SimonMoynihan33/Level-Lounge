850753461fb52041bfdca4fd809bcf0e8b6d8dc1
Simon Moynihan <simon.moynihan33@gmail.com>
2024-09-25 11:11:41 +0000

    Push test: Explicitly set cloudinary configs in settings

--- my_project/settings.py
+++ my_project/settings.py
@@ -35,6 +35,13 @@ CLOUDINARY_STORAGE = {
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
+cloudinary.config(
+    cloud_name=os.getenv('CLOUDINARY_CLOUD_NAME'),
+    api_key=os.getenv('CLOUDINARY_API_KEY'),
+    api_secret=os.getenv('CLOUDINARY_API_SECRET'),
+)
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
